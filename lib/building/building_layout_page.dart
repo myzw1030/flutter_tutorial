ac8b23c10fd680660857802684703864d5678c87
miyazawa <13tmtmtommy@gmail.com>
2023-11-01 15:28:50 +0900

    Add interactivity to your Flutter app

--- lib/building/building_layout_page.dart
+++ lib/building/building_layout_page.dart
@@ -1,5 +1,3 @@
-// import 'dart:js';
-
 import 'package:flutter/material.dart';
 
 class BuildingLayoutPage extends StatelessWidget {
@@ -13,9 +11,7 @@ class BuildingLayoutPage extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Image.asset(
-            'images/lake.jpg',
-          ),
+          Image.asset('images/lake.jpg'),
           const TitleSection(),
           const ButtonSection(),
           const TextSection(),
@@ -56,11 +52,7 @@ class TitleSection extends StatelessWidget {
               ],
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
+          const FavoriteWidget(),
         ],
       ),
     );
@@ -112,7 +104,7 @@ class _buildButtonColumn extends StatelessWidget {
               color: color,
             ),
           ),
-        )
+        ),
       ],
     );
   }
@@ -134,6 +126,60 @@ class TextSection extends StatelessWidget {
         'enjoyed here include rowing, and riding the summer toboggan run.',
         softWrap: true,
       ),
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  // お気に入り数とお気に入りチェック
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited
+                ? Icon(
+                    Icons.star,
+                    color: Colors.red[500],
+                  )
+                : const Icon(Icons.star_border)),
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
     );
   }
 }
