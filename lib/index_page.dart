708c26d9ce640b7c512dd69554f0a08c147a6805
miyazawa <13tmtmtommy@gmail.com>
2023-11-06 18:22:46 +0900

    Tutorial2.2 - 次はFlutter製アプリの画面模写をしてみよう。

--- lib/index_page.dart
+++ lib/index_page.dart
@@ -1,35 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorial/building/building_layout_page.dart';
+import 'package:flutter_tutorial/residence/residence_page.dart';
 
 class IndexPage extends StatelessWidget {
   const IndexPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => BuildingLayoutPage(),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                shape: const StadiumBorder(),
-              ),
-              child: const Text(
-                'Tutorial1',
-              ),
+            PageButton(
+              text: 'Tutorial1',
+              widget: BuildingLayoutPage(),
+            ),
+            PageButton(
+              text: 'Tutorial2.2',
+              widget: ResidencePage(),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+// 各チュートリアルのページ遷移ボタン
+class PageButton extends StatelessWidget {
+  const PageButton({
+    required this.text,
+    required this.widget,
+    super.key,
+  });
+
+  final Widget widget;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => widget,
+          ),
+        );
+      },
+      child: Text(text),
     );
   }
 }
